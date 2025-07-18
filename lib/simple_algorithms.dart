c3161b9248aedd880144ca3f36cfd68f5d770249
Dzulkifli Anwar <dzulkiflianwar2@gmail.com>
2025-07-18 13:19:02 +0700

    feat: add module

--- lib/simple_algorithms.dart
+++ lib/simple_algorithms.dart
@@ -1,7 +1,10 @@
 library;
 
 export 'caesar_cipher.dart';
+export 'factorial.dart';
 export 'fibonacci.dart';
+export 'number_theory_utils.dart';
 export 'palindrome.dart';
 export 'prime_number.dart';
 export 'roman_conversion.dart';
+export 'temperature_conversion.dart';
